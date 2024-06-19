fe89dd450fd0a6cfe65c65e574024297a002201f
Ramraj Patil <ramrajpatil24@gmail.com>
2024-06-19 09:28:19 +0530

    added e-r-diagram in db-resources

--- grofers-backend/src/main/java/com/grofers/pojos/Order.java
+++ grofers-backend/src/main/java/com/grofers/pojos/Order.java
@@ -14,7 +14,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
